4229339b147d1483679ef2214f4c7e8fb6fb8b62
Niel de la Rouviere <nieldlr@gmail.com>
2018-01-08 12:15:29 +0200

    Initial commit

---
+++ src/eventPage.js
@@ -0,0 +1,17 @@
+chrome.alarms.create('fetch_gas_price',{
+  "periodInMinutes": 5
+});
+
+chrome.alarms.onAlarm.addListener(alarm => {
+  fetchGasPrice();
+});
+
+function fetchGasPrice() {
+  fetch("https://ethgasstation.info/json/ethgasAPI.json")
+    .then((res) => {return res.json()})
+    .then(data => {
+      chrome.browserAction.setBadgeText({text: String(parseInt(data.average, 10)/10)});
+    });
+}
+
+fetchGasPrice(); // Initial fetch
