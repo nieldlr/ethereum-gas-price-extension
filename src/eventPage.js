0be9cd9fe9e69d3487cafa9d18db998b3b7626bf
Niel de la Rouviere <nieldlr@gmail.com>
2020-10-14 12:11:14 +0200

    Add firefox specific proxy

--- src/eventPage.js
+++ src/eventPage.js
@@ -20,7 +20,18 @@ function updateBadge() {
 }
 
 function fetchGasPrice() {
-  const url = "https://ethgasstation.info/api/ethgasAPI.json?api-key=d216b81e8ed8f5c8a82744be99b22b2d1757098f40df3c2ea5bb40b3912b";
+  
+  // const url = "https://ethgasstation.info/api/ethgasAPI.json?api-key=d216b81e8ed8f5c8a82744be99b22b2d1757098f40df3c2ea5bb40b3912b";
+  // return fetch(url)
+  //   .then((res) => {return res.json()})
+  //   .then(data => {
+  //     // Store the current data for the popup page
+  //     appData.ethGasStationData = data;
+  //     // Update badge
+  //     updateBadge();
+  //   });
+
+  const url = "https://gasprice-proxy.herokuapp.com/"; // Firefox Proxy
   return fetch(url)
     .then((res) => {return res.json()})
     .then(data => {
